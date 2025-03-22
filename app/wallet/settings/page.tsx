ccc9fe6f3a951b06695756e4446b11c748c506c7
cresxjohn <cresellejohn@gmail.com>
2025-03-22 08:37:07 +0800

    Fix eslint

--- app/wallet/settings/page.tsx
+++ app/wallet/settings/page.tsx
@@ -11,32 +11,30 @@ import { SidebarInset, SidebarTrigger } from "@/components/ui/sidebar";
 
 export default function Page() {
   return (
-    <>
-      <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2">
-          <div className="flex items-center gap-2 px-4">
-            <SidebarTrigger className="-ml-1" />
-            <Separator orientation="vertical" className="mr-2 h-4" />
-            <Breadcrumb>
-              <BreadcrumbList>
-                <BreadcrumbItem className="hidden md:block">
-                  <BreadcrumbLink href="/wallet">Wallet</BreadcrumbLink>
-                </BreadcrumbItem>
-                <BreadcrumbSeparator className="hidden md:block" />
-                <BreadcrumbItem>
-                  <BreadcrumbPage>Settings</BreadcrumbPage>
-                </BreadcrumbItem>
-              </BreadcrumbList>
-            </Breadcrumb>
-          </div>
-        </header>
-        <div className="px-6">
-          <div className="mb-4">
-            <p className="text-2xl font-bold mb-1">Settings</p>
-            <p className="text-sm font-light">Personalize your wallet.</p>
-          </div>
+    <SidebarInset>
+      <header className="flex h-16 shrink-0 items-center gap-2">
+        <div className="flex items-center gap-2 px-4">
+          <SidebarTrigger className="-ml-1" />
+          <Separator orientation="vertical" className="mr-2 h-4" />
+          <Breadcrumb>
+            <BreadcrumbList>
+              <BreadcrumbItem className="hidden md:block">
+                <BreadcrumbLink href="/wallet">Wallet</BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator className="hidden md:block" />
+              <BreadcrumbItem>
+                <BreadcrumbPage>Settings</BreadcrumbPage>
+              </BreadcrumbItem>
+            </BreadcrumbList>
+          </Breadcrumb>
         </div>
-      </SidebarInset>
-    </>
+      </header>
+      <div className="px-6">
+        <div className="mb-4">
+          <p className="text-2xl font-bold mb-1">Settings</p>
+          <p className="text-sm font-light">Personalize your wallet.</p>
+        </div>
+      </div>
+    </SidebarInset>
   );
 }
