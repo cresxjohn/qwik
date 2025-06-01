cf58e4b7ab3f8a4e71fd65f62550072fce3ca969
cres <cresellejohn@gmail.com>
2025-06-02 01:11:38 +0800

    Set header as sticky

--- app/wallet/settings/page.tsx
+++ app/wallet/settings/page.tsx
@@ -12,7 +12,7 @@ import { SidebarInset, SidebarTrigger } from "@/components/ui/sidebar";
 export default function Page() {
   return (
     <SidebarInset>
-      <header className="flex h-16 shrink-0 items-center gap-2">
+      <header className="sticky top-0 z-10 bg-background rounded-4xl flex h-16 shrink-0 items-center gap-2">
         <div className="flex items-center gap-2 px-4">
           <SidebarTrigger className="-ml-1" />
           <Separator orientation="vertical" className="mr-2 h-4" />
